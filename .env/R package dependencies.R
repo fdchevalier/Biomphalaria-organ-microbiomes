ec9cd8bdece18c84d4ffc291bb198f55da2bd4a5
fdchevalier <fcheval@txbiomed.org>
2020-05-14 14:55:38 -0500

    Update R dependencies (filename and content)

---
+++ .env/R package dependencies.R
@@ -0,0 +1,31 @@
+# System
+options("repos" = c(CRAN = "https://cran.revolutionanalytics.com"))
+library("devtools")
+library("BiocManager")
+
+install_github("jalvesaq/colorout", ref="7ea9440", upgrade="never")
+
+
+##install_version("doParallel", version="1.0.15", upgrade="never")
+
+##library(rcompanion)
+##library(multcompView)
+
+
+#install("microbiome", update=FALSE)
+#install("phyloseq", update=FALSE)
+#install("picante", update=FALSE)          # PD function
+#install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis", ref="6e09713", upgrade="never")
+#install_github("ropensci/taxa", ref="49969dc", upgrade="never")
+##library("metacoder")
+#install_version("phylogram", version="2.1.0", upgrade="never")      # prune function
+
+## Graph
+##library("RColorBrewer")
+##library("colorspace")
+#install_version("gridBase", version="0.4-7", upgrade="never")
+#install_version("plotrix", version"3.7-5", upgrade="never")
+#install_version("ggpubr", version="0.2.4", upgrade="never")
+#install_version("ggdendro", version="0.1-20", upgrade="never")
+
+
