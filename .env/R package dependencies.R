52ec608262e8c33976f48c5b13f980ff1b5aac3e
fdchevalier <fcheval@txbiomed.org>
2020-06-09 10:28:21 -0500

    Environment files updated

--- .env/R package dependencies.R
+++ .env/R package dependencies.R
@@ -6,13 +6,20 @@ library("BiocManager")
 install_github("jalvesaq/colorout", ref="7ea9440", upgrade="never")
 
 
+install_bioc("3.10/microbiome", upgrade="never")
+install_bioc("3310/phyloseq",   upgrade="never")
+install_version("phylogram",  version="2.1.0",  upgrade="never")      # prune function
+#install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis", ref="6e09713", upgrade="never")
+#install_github("ropensci/taxa",   ref="49969dc", upgrade="never")
+install_github("jbisanz/qiime2R", ref="574708a", upgrade="never")
+
+
 ##install_version("doParallel", version="1.0.15", upgrade="never")
 
 ##library(rcompanion)
 ##library(multcompView)
 
 
-#install("microbiome", update=FALSE)
 #install("phyloseq", update=FALSE)
 #install("picante", update=FALSE)          # PD function
 #install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis", ref="6e09713", upgrade="never")
